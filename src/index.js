57a9ed333abc0412913df08d478fd63992f29af0
AmmanRizwan <ammanrizwan00007@gmail.com>
2024-12-10 13:01:27 +0530

    last commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ require('./config/code.config.js');
 const express = require('express');
 const serverless = require('serverless-http');
 const cors = require('cors');
-const port = process.env.PORT;
 const Router = require('./routes/code.routes.js');
 
 index = express();
@@ -20,7 +19,5 @@ index.use('/api/userdata', Router);
 index.get('/', (req, res) => {
   res.json({message: "Python Practice Server is Running"});
 })
-
-index.listen(port, () => console.log(`index is still running on port:${port}\nListening: http://localhost:${port}`));
 
 module.exports.handler = serverless(index);
